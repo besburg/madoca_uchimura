555763b6f4e1d7f3a6e80e37dbb46d8668388f12
tk <kmnswimert@gmail.com>
2020-04-09 01:01:28 +0900

    Swiper操作途中

--- js/main.js
+++ js/main.js
@@ -1,12 +1,59 @@
-var mySwiper = new Swiper ('.swiper-container', {
+const mySwiper = new Swiper ('.swiper-container', {
     direction: 'vertical',
     effect: 'slide',
     slidesPerView: 1,
     loop: true,
     autoplay: {
-        delay: 8000,
+        delay: 3000,
     },
     mousewheel: {
         invert: false,
     },
+})
+
+const mySwiper2 = new Swiper ('.swiper-container2', {
+    direction: 'vertical',
+    effect: 'slide',
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+    mousewheel: {
+        invert: false,
+    },
+})
+
+const mySwiper3 = new Swiper ('.swiper-container3', {
+    direction: 'vertical',
+    effect: 'slide',
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+    },
+    mousewheel: {
+        invert: false,
+    },
+})
+
+// $('.portrait').on('click',function(){
+//     mySwiper.slideTo(0, 100);
+//     })
+    
+$(function(){
+    $('.swiper-container2').hide();
+    $('.swiper-container3').hide();
+    $('.portrait-click').on('click',function(){
+        $('.swiper-container').hide();
+        $('.swiper-container3').hide();
+        $('.swiper-container2').show();
+        mySwiper2.slideTo(0, 100);
+    })
+    $('.landscape-click').on('click',function(){
+        $('.swiper-container').hide();
+        $('.swiper-container2').hide();
+        $('.swiper-container3').show();
+        mySwiper3.slideTo(0, 100);
+    })
 })
